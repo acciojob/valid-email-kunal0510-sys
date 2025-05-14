9bf0a2193427023076a3ae0366a5561ec407ec76
kunal0510-sys <kunalr2205@gmail.com>
2025-05-14 13:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function validEmail(str) {
-  //your JS code here.
+  // Regular expression to validate email format
+  const regex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
